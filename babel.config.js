f5634cac1294368231bfe44cd8af92122af34a7a
Johnny <108516218+johnnyrpy@users.noreply.github.com>
2026-06-01 14:05:28 +0200

    chore(BP-4534): update expo 54.0.10 (#8)
    * chore(BP-4534): update expo 53.0.27
    
    * expo 54

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-// reanimated babel plugin: https://stackoverflow.com/a/71754691 
+// Reanimated v4 uses the worklets plugin, which must be listed last.
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin",
+      'react-native-worklets/plugin',
     ],
   };
 };
